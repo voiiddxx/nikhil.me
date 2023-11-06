0cbb90afc46234dab03e71477db111dee7e00da5
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-11-06 11:15:15 +0530

    responsiveness

--- src/View/about/About.jsx
+++ src/View/about/About.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import "./About.css"
 import abouthero from "./stage1.jpg"
 const About = () => {
-  return (
+  return ( 
     <div className="about-main">
         <div className="about-text">
           <h1>About Me</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas debitis natus excepturi tempore minima cum, mollitia aut quaerat autem ratione praesentium ea, error corrupti odit aperiam nesciunt! Possimus mollitia fugiat nemo maxime vitae, itaque corporis similique deserunt tenetur obcaecati, sunt adipisci voluptate nisi qui odio incidunt ut nihil molestiae magnam eos officia? Repellat, ipsa amet officia quo dicta soluta magni sunt error nihil nemo pariatur minima, nostrum quae possimus fugit numquam, quasi dolores cupiditate ad doloribus! Minus earum quam laboriosam! </p>
+          <p>Hello! My name is Nikhil Kumar and I enjoy creating things that live on the internet. My interest in App devlopment and creating web/app designs started back in 2021 when I decided to try to learn some app and website devlopment— turns out into a habbit
+Fast-forward to today, and I’ve had the privilege of making some good mobile application and web application with clean and minimal function and ui and still learning to make myself better </p>
         </div>
         <div className="about-image">
     <img src={abouthero} alt="aboutherosvg" />
