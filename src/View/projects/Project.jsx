176f6a859399292f8c6b6ffcec900b680820faaa
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2024-01-07 16:36:14 +0530

    changes in ui again

--- src/View/projects/Project.jsx
+++ src/View/projects/Project.jsx
@@ -10,31 +10,93 @@ const Project = () => {
             </div>
             <div className="project-card">
               {/* Pro Planet Card */}
-              <div className="card-box">
-                <a href="https://github.com/voiiddxx/pro-planet-server-">
-                <img src="https://cdn3d.iconscout.com/3d/premium/thumb/earth-7865127-6308176.png" alt="project" />
-                </a>
-                <h2>Pro Planet</h2>
-                <p>React Js , Nodejs , MongoDB, <br />
-CSS , Express js , CLoudinary</p>
+              <div className="p-card">
+                <div className="p-image"></div>
+              <div className="p-text">
+              <h1>Planet Pulse</h1>
+              <p>Platform made for identify the enciroementol cincious poeple and to engage them to accelrate</p>
+              <div className="tech">
+                <div className="tech-box">
+                  <p>React Js</p>
+                </div>
+                <div className="tech-box">
+                  <p>Nodejs</p>
+                </div>
+                <div className="tech-box">
+                  <p>Mongo Db</p>
+                </div>
+                <div className="tech-box">
+                  <p>Context API</p>
+                </div>
+                <div className="tech-box">
+                  <p>Express Js</p>
+                </div>
+                <div className="tech-box">
+                  <p>Figma</p>
+                </div>
               </div>
-              {/* Pro Planet Card end */}
-              <div className="card-box2">
-              <a href="https://github.com/voiiddxx/Thread-react-node">
-              <img src="https://cdn3d.iconscout.com/3d/free/thumb/free-meta-10193096-8250233.png" alt="project" />
-              </a>
-                <h2>Thread Clone</h2>
-                <p>React Js , Nodejs , MongoDB, <br />
-CSS , Express js , CLoudinary</p>
               </div>
-              <div className="card-box3">
-              <a href="https://github.com/voiiddxx/bazaar">
-              <img src="https://cdn3d.iconscout.com/3d/premium/thumb/man-getting-a-discount-coupon-7743076-6241784.png?f=webp" alt="project" />
-              </a>
-                <h2>Bazaar</h2>
-                <p>Flutter , Dart , Mongoose , <br /> Nodejs 
-, Express js , CLoudinary</p>
+
               </div>
+              <div className="p-card">
+                <div className="p-image-2"></div>
+                <div className="p-text">
+              <h1>Planet Pulse</h1>
+              <p>Platform made for identify the enciroementol cincious poeple and to engage them to accelrate</p>
+              <div className="tech">
+                <div className="tech-box">
+                  <p>React Js</p>
+                </div>
+                <div className="tech-box">
+                  <p>Nodejs</p>
+                </div>
+                <div className="tech-box">
+                  <p>Mongo Db</p>
+                </div>
+                <div className="tech-box">
+                  <p>Context API</p>
+                </div>
+                <div className="tech-box">
+                  <p>Express Js</p>
+                </div>
+                <div className="tech-box">
+                  <p>Figma</p>
+                </div>
+              </div>
+              </div>
+
+              </div>
+              <div className="p-card">
+                <div className="p-image-3"></div>
+                <div className="p-text">
+              <h1>Planet Pulse</h1>
+              <p>Platform made for identify the enciroementol cincious poeple and to engage them to accelrate</p>
+              <div className="tech">
+                <div className="tech-box">
+                  <p>React Js</p>
+                </div>
+                <div className="tech-box">
+                  <p>Nodejs</p>
+                </div>
+                <div className="tech-box">
+                  <p>Mongo Db</p>
+                </div>
+                <div className="tech-box">
+                  <p>Context API</p>
+                </div>
+                <div className="tech-box">
+                  <p>Express Js</p>
+                </div>
+                <div className="tech-box">
+                  <p>Figma</p>
+                </div>
+              </div>
+              </div>
+
+              </div>
+             
+             
+            
              
               
             </div>
