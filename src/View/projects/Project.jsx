1e540a13a4524bc18ad96f607d296d17a3e04342
voiiddxx <7087330257n@gmail.com>
2024-01-07 19:50:39 +0530

    card chasnges

--- src/View/projects/Project.jsx
+++ src/View/projects/Project.jsx
@@ -5,12 +5,43 @@ const Project = () => {
     <div className="project-main">
         <div className="project-box">
             <div className="project-text">
+              
                 <h1>Checkout The Premium <br /> Projects and Explore</h1>
                 <p>What good is that Skill if you can't put it to use on the project?</p>
             </div>
             <div className="project-card">
               {/* Pro Planet Card */}
-              <div className="p-card">
+            
+                            <div className="p-card">
+                              <div className="p-image-2"></div>
+                              <div className="p-text">
+                            <h1>Planet Pulse</h1>
+                            <p>Platform made for identify the enciroementol cincious poeple and to engage them to accelrate</p>
+                            <div className="tech">
+                              <div className="tech-box">
+                                <p>React Js</p>
+                              </div>
+                              <div className="tech-box">
+                                <p>Nodejs</p>
+                              </div>
+                              <div className="tech-box">
+                                <p>Mongo Db</p>
+                              </div>
+                              <div className="tech-box">
+                                <p>Context API</p>
+                              </div>
+                              <div className="tech-box">
+                                <p>Express Js</p>
+                              </div>
+                              <div className="tech-box">
+                                <p>Figma</p>
+                              </div>
+                            </div>
+                            </div>
+                            </div>
+
+
+                            <div className="p-card">
                 <div className="p-image"></div>
               <div className="p-text">
               <h1>Planet Pulse</h1>
@@ -39,7 +70,7 @@ const Project = () => {
 
               </div>
               <div className="p-card">
-                <div className="p-image-2"></div>
+                <div className="p-image-3"></div>
                 <div className="p-text">
               <h1>Planet Pulse</h1>
               <p>Platform made for identify the enciroementol cincious poeple and to engage them to accelrate</p>
@@ -67,7 +98,35 @@ const Project = () => {
 
               </div>
               <div className="p-card">
-                <div className="p-image-3"></div>
+                <div className="p-image-4"></div>
+                <div className="p-text">
+              <h1>Planet Pulse</h1>
+              <p>Platform made for identify the enciroementol cincious poeple and to engage them to accelrate</p>
+              <div className="tech">
+                <div className="tech-box">
+                  <p>React Js</p>
+                </div>
+                <div className="tech-box">
+                  <p>Nodejs</p>
+                </div>
+                <div className="tech-box">
+                  <p>Mongo Db</p>
+                </div>
+                <div className="tech-box">
+                  <p>Context API</p>
+                </div>
+                <div className="tech-box">
+                  <p>Express Js</p>
+                </div>
+                <div className="tech-box">
+                  <p>Figma</p>
+                </div>
+              </div>
+              </div>
+
+              </div>
+              <div className="p-card">
+                <div className="p-image-5"></div>
                 <div className="p-text">
               <h1>Planet Pulse</h1>
               <p>Platform made for identify the enciroementol cincious poeple and to engage them to accelrate</p>
